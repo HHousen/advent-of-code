77bc1f57c5d6aaf1426af9af6ee6b307f617ebab
Hayden Housen <hayden@haydenhousen.com>
2025-11-30 23:17:25 -0500

    prepare for day 1

--- 2025/README.md
+++ 2025/README.md
@@ -1,1 +1,3 @@
-# Coming soon :)
+# HHousen Advent Of Code 2025
+
+> HHousen's solutions to the 2025 Advent Of Code puzzles at <https://adventofcode.com/2025>.
---
+++ 2025/day_1/day1.py
@@ -0,0 +1,18 @@
+
+
+with open("puzzle_input.txt", "r") as puzzle_input:
+    puzzle_input = puzzle_input.read().splitlines()
+
+
+
+part1_solution = None
+
+# Part 1 Solution: 
+print(f"Part 1 Solution: {part1_solution}")
+
+
+
+part2_solution = None
+
+# Part 2 Solution: 
+print(f"Part 2 Solution: {part2_solution}")
